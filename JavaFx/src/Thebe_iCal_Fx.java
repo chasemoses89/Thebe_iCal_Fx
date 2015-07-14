732370dbd3dd2abf0999f422e9aee4227b07f2da
chasemoses89 <chaselee@hawaii.edu>
2015-07-14 11:04:26 -1000

    Update Thebe_iCal_Fx.java

--- JavaFx/src/Thebe_iCal_Fx.java
+++ JavaFx/src/Thebe_iCal_Fx.java
@@ -20,7 +20,10 @@ public class Thebe_iCal_Fx extends Application {
         
     Button button = new Button ("save");
     Label notification = new Label ();
+    
     TextField subject = new TextField("");
+    TextField fGeoLat = new TextField("");
+    TextField fGeoLong = new TextField("");
     TextField location = new TextField("");
     TextArea description = new TextArea ("");
     //Event subject line
@@ -50,83 +53,110 @@ public class Thebe_iCal_Fx extends Application {
   	//Event class
   	String sClass = "";
   	//User's choice
-  	int iChoice;
-  	
+  	Float GeoLat = 21.4667f;
+	//latitude of location
+	Float GeoLong = 157.9833f;
+	//User's choice for window 1
+	int iChoice = -1;
+	//sentinel value for loops
+	boolean bValid = false;
+	//indicates if a valid geographic position was entered
+	boolean bGeoPos = false;
+	//states if end date/time is less than the start date/time
+	boolean bValidTimeFrame;
+	//states if day is valid based on month an leap year
+	boolean bValidDate;  	
     
-    @Override public void start(final Stage stage) {
+	@Override
+    public void start(final Stage stage) {
         stage.setTitle("Thebe_iCal");
         Scene scene = new Scene(new Group(), 450, 250);
-        
+       
         //comboBox for start of month
         final ComboBox<String> sMonthsComboBox = new ComboBox<String>();
-        sMonthsComboBox.getItems().addAll(
-        		"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
-        );
+        sMonthsComboBox.getItems().addAll("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" );
+        //set start of month
+        sMonthsComboBox.setValue("1");
         
         //comboBox for start of day
         final ComboBox<String> sDaysComboBox = new ComboBox<String>();
         sDaysComboBox.getItems().addAll(
         		"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
         );
+        //set value for start day
+        sDaysComboBox.setValue("1");
         
         //comboBox for start of year
         final ComboBox<String> sYearsComboBox = new ComboBox<String>();
         sYearsComboBox.getItems().addAll(
         		"2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"
-        );
+        );       
+        //set value for start year
+        sYearsComboBox.setValue("2015");
         
         //comboBox for start of time
         final ComboBox<String> sTimeComboBox = new ComboBox<String>();
         sTimeComboBox.getItems().addAll(
-        		"0000", "0100", "0200", "0300", "0400", "0500", "0600", "0700", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300", "2400"
-        );
+        		"0100", "0200", "0300", "0400", "0500", "0600", "0700", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300", "2400"
+        );     
+        //set value for start time
+        sTimeComboBox.setValue("0100");
     
         //comboBox for end of month
         final ComboBox<String> eMonthsComboBox = new ComboBox<String>();
         eMonthsComboBox.getItems().addAll(
         		"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
         );
+        //set value for end of month
+        sMonthsComboBox.setValue("1");
         
       //comboBox for end of day
         final ComboBox<String> eDaysComboBox = new ComboBox<String>();
         eDaysComboBox.getItems().addAll(
         		"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
         );
+        //set value for end day
+        eDaysComboBox.setValue("1");
         
       //comboBox for end of year
         final ComboBox<String> eYearsComboBox = new ComboBox<String>();
         eYearsComboBox.getItems().addAll(
         		"2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"
         );
+        //set value for end of year
+        eYearsComboBox.setValue("2015");
         
       //comboBox for end of time
         final ComboBox<String> eTimeComboBox = new ComboBox<String>();
         eTimeComboBox.getItems().addAll(
-        		"0000", "0100", "0200", "0300", "0400", "0500", "0600", "0700", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300", "2400"
+        		"0100", "0200", "0300", "0400", "0500", "0600", "0700", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300", "2400"
         );
+        //set value end time
+        eTimeComboBox.setValue("0100");
         
+        //comboBox for class
         final ComboBox<String> eventClassComboBox = new ComboBox<String>();
         eventClassComboBox.getItems().addAll(
         		"Public", "Private", "Confidential"
         );
-        
+        sClass = eventClassComboBox.getValue();
     	sSubject = subject.getText();
     	sLocation = location.getText();
     	sDescription = description.getText();
-    	startYear = sYearsComboBox.getSelectionModel().toString();
-    	startMonth = sMonthsComboBox.getSelectionModel().toString();
-    	startDay = sDaysComboBox.getSelectionModel().toString();
-    	startTime = sTimeComboBox.getSelectionModel().toString();
-    	endYear = eYearsComboBox.getSelectionModel().toString();
-    	endMonth = eMonthsComboBox.getSelectionModel().toString();
-    	endDay = eDaysComboBox.getSelectionModel().toString();
-    	endTime = eTimeComboBox.getSelectionModel().toString();
-    	sClass = eventClassComboBox.getSelectionModel().toString();
-        
+    	startYear = sYearsComboBox.getValue();
+    	startMonth = sMonthsComboBox.getValue();
+    	startDay = sDaysComboBox.getValue();
+    	startTime = sTimeComboBox.getValue();
+    	endYear = eYearsComboBox.getValue();
+    	endMonth = eMonthsComboBox.getValue();
+    	endDay = eDaysComboBox.getValue();
+    	endTime = eTimeComboBox.getValue();
+    	sClass = eventClassComboBox.getValue();
+    	
         GridPane grid = new GridPane();      
         grid.setVgap(4);
         grid.setHgap(4);
-        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setPadding(new Insets(20, 20, 20, 20));
         grid.add(new Label("Subject: "), 0, 0);
         grid.add(subject,  0,1);
         grid.add(new Label("Start Month: "), 0, 2);
@@ -149,10 +179,15 @@ public class Thebe_iCal_Fx extends Application {
         grid.add(new Label("Event Class: "), 2,0);
         grid.add(eventClassComboBox, 2,1);
         
-        grid.add(new Label("Description:"), 0, 10);
-        grid.add(description, 0, 11);
+        grid.add(new Label("Description:"), 0, 14);
+        grid.add(description, 0, 15);
         
-        grid.add(button, 0, 12);
+        grid.add(new Label("Latitude:"), 0, 10);
+        grid.add(fGeoLat, 0, 11); 
+        grid.add(new Label("Longitude:"), 0, 12);
+        grid.add(fGeoLong, 0, 13);
+        
+        grid.add(button, 0, 16);
         grid.add (notification, 1, 3, 3, 1);
         
         Group root = (Group)scene.getRoot();
@@ -160,8 +195,8 @@ public class Thebe_iCal_Fx extends Application {
         stage.setScene(scene);
         stage.show();
         
+          
         button.setOnAction(new EventHandler <ActionEvent >() {
-         	  @Override
          	  public void handle(ActionEvent actionEvent) {
          		 FileChooser fileChooser = new FileChooser();
       	     File direct = new File(System.getProperty("user.home"));
@@ -194,9 +229,9 @@ public class Thebe_iCal_Fx extends Application {
             				+ "CLASS:" + sClass + "\n"
             				+ "CREATED:20150627T031544Z\n"
             				+ "DESCRIPTION:" + sDescription + "\\n\n"
-            				+ "DTEND;TZID=\"Hawaiian Standard Time\":" + eYearsComboBox + eMonthsComboBox + sDaysComboBox + "T" + eTimeComboBox + "00\n"
+            				+ "DTEND;TZID=\"Hawaiian Standard Time\":" + eYearsComboBox + eMonthsComboBox + eDaysComboBox + "T" + eTimeComboBox + "00\n"
             				+ "DTSTAMP:20150627T031544Z\n"
-            				+ "DTSTART;TZID=\"Hawaiian Standard Time\":" + sYearsComboBox + sMonthsComboBox + eDaysComboBox + "T" + sTimeComboBox + "00\n"
+            				+ "DTSTART;TZID=\"Hawaiian Standard Time\":" + sYearsComboBox + sMonthsComboBox + sDaysComboBox + "T" + sTimeComboBox + "00\n"
             				+ "LAST-MODIFIED:20150627T031544Z\n"
             				+ "LOCATION:" + sLocation + "\n"
             				+ "PRIORITY:5\n"
@@ -205,25 +240,8 @@ public class Thebe_iCal_Fx extends Application {
             				+ "TRANSP:OPAQUE\n"
             				+ "UID:040000008200E00074C5B7101A82E00800000000407663BC33B0D001000000000000000\n"
             				+ "\t010000000109752F585EB3B448B59159DFD3CF4D2\n"
-            				//+ "X-ALT-DESC;FMTTYPE=text/html:<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2//E\n"
-            				//+ "\tN\">\\n<HTML>\\n<HEAD>\\n<META NAME=\"Generator\" CONTENT=\"MS Exchange Server ve\n"
-            				//+ "\trsion 14.02.5004.000\">\\n<TITLE></TITLE>\\n</HEAD>\\n<BODY>\\n<!-- Converted f\n"
-            				//+ "\trom text/rtf format -->\\n\\n<P DIR=LTR><SPAN LANG=\"en-us\"><FONT FACE=\"Calib\n"
-            				//+ "\tri\">This is an example of sample text in the body of an appointment.</FONT\n"
-            				//+ "\t></SPAN><SPAN LANG=\"en-us\"> </SPAN></P>\\n\\n</BODY>\\n</HTML>\n"
-            				//+ "X-MICROSOFT-CDO-BUSYSTATUS:BUSY\n"
-            				//+ "X-MICROSOFT-CDO-IMPORTANCE:1\n"
-            				//+ "X-MICROSOFT-DISALLOW-COUNTER:FALSE\n"
-            				//+ "X-MS-OLK-AUTOFILLLOCATION:FALSE\n"
-            				//+ "X-MS-OLK-CONFTYPE:0\n"
-            				//+ "BEGIN:VALARM\n"
-            				//+ "TRIGGER:-PT15M\n"
-            				//+ "ACTION:DISPLAY\n"
-            				//+ "DESCRIPTION:Reminder\n"
-            				//+ "END:VALARM\n"
             				+ "END:VEVENT\n"
             				+ "END:VCALENDAR");     		
-   
               	    		fout.close();
                    }
               	 catch (IOException ex) {
